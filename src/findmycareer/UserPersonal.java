06b292ab1d28057b0c4bec26b4c1742c41b07909
7107723813 <7107723813@CCI-12953.tafe.mst>
2015-06-16 15:57:40 +1000

    User.java added with the subclasses UserAdmin and UserPersonal

---
+++ src/findmycareer/UserPersonal.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package findmycareer;
+
+/**
+ *
+ * @author 7107723813
+ */
+public class UserPersonal extends User {
+    
+}
